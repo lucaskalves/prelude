c6c5d29cbfb3d7230618a46220127291205341a4
Lucas Alves <lucaskalves@gmail.com>
2016-12-13 21:22:53 -0200

    added custom init.el

---
+++ personal/init.el
@@ -0,0 +1,5 @@
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+
+(setq dotspacemacs-elpa-https nil)
